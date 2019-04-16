337dcfb549b59e9c9ddfd62a92d80fb0cffca5b5
cqu2014 <286931986@qq.com>
2019-04-16 16:53:26 +0800

    out data

--- src/main/java/com/oliver/guava/mapper/TemplatePoMapper.java
+++ src/main/java/com/oliver/guava/mapper/TemplatePoMapper.java
@@ -1,6 +1,9 @@
 package com.oliver.guava.mapper;
 
 import com.oliver.guava.dao.TemplatePo;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 @Component("templatePoMapper")
@@ -18,4 +21,8 @@ public interface TemplatePoMapper {
     int updateByPrimaryKeyWithBLOBs(TemplatePo record);
 
     int updateByPrimaryKey(TemplatePo record);
+
+    @Select("select * from system_email where brand = #{brand} and email_type = #{emailType}")
+    @ResultMap("ResultMapWithBLOBs")
+    TemplatePo queryTemplate(@Param("brand") String brand, @Param("emailType") String emailType);
 }
